86117b63a9fc1238c324cc931edc7b931ec40aa9
Danil-Slastenov <st9309922@stud.duikt.edu.ua>
2024-10-02 12:05:05 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,1 +1,17 @@
-1234
+int main() {
+    int age;
+    cout << "Введіть ваш вік: ";
+    cin >> age;
+
+    if (age >= 0 && age <= 17) {
+        cout << "Ви належите до молоді." << endl;
+    } else if (age >= 18 && age <= 65) {
+        cout << "Ви належите до дорослих." << endl;
+    } else if (age >= 66) {
+        cout << "Ви належите до людей похилого віку." << endl;
+    } else {
+        cout << " Повторіть ще раз." << endl;
+    }
+
+    return 0;
+}
